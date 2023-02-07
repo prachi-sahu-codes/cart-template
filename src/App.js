c6fe53f5a4e4830a1e266a7233936d74f006122f
SahuPrachi <prachisahu998@gmail.com>
2023-02-07 19:01:04 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello!!!</p>
+        <div className="content">
+          <button>+</button>
+          <p>item</p>
+          <button>-</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
